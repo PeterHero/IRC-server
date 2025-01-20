50816d6f25cfd653448c5ab271c59811ed19239b
Petr Hrdina <phrdina@proton.me>
2025-01-20 13:46:20 +0100

    Handle multiple connections at once

---
+++ src/main/java/cz/cuni/mff/hrdinap1/ircserver/IRCServer.java
@@ -0,0 +1,5 @@
+package cz.cuni.mff.hrdinap1.ircserver;
+
+public class IRCServer {
+    // this will be the class that will hold all data about server, its users, channels and so on
+}
--- src/main/java/cz/cuni/mff/hrdinap1/ircserver/Main.java
+++ src/main/java/cz/cuni/mff/hrdinap1/ircserver/Main.java
@@ -5,27 +5,24 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int serverPort = 6667;
 
-        try (ServerSocket s = new ServerSocket(6667)) {
+        try (ServerSocket s = new ServerSocket(serverPort)) {
             System.out.println("Server ready");
-            try (Socket socket = s.accept()) {
-                InputStream in = socket.getInputStream();
-                OutputStream out = socket.getOutputStream();
-                while (true) {
-                    int b = in.read();
-                    if (b == -1) {
-                        break;
-                    }
 
-                    out.write(b);
+            try (var executor = Executors.newWorkStealingPool()) {
+                while (true) {
+                    Socket socket = s.accept();
+                    executor.submit(new ServiceRequest(socket));
                 }
             }
 
         } catch (IOException e) {
+            System.out.println("Error could not start server on port" + serverPort);
             throw new RuntimeException(e);
         }
     }
