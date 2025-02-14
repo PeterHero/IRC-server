cc35eced5e1487e5692d153ea8ec88ea975a7600
Petr Hrdina <phrdina@proton.me>
2025-02-14 11:39:36 +0100

    Add package comment and similar

---
+++ src/main/java/cz/cuni/mff/hrdinap1/ircserver/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides minimal IRC server implementation. The server can service multiple users at once (obviously).
+ * Users that join the server can join and leave channels a message any user or channel they joined.
+ * This is a project created for the Java class on MFF CUNI.
+ * Created by Petr Hrdina in year 2025.
+ */
+package cz.cuni.mff.hrdinap1.ircserver;
